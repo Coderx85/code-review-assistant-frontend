436ec0032a976a7dfa1b23a23ef27a0566d4dd20
Coderx85 <work.priyanshu085@gmail.com>
2025-06-03 18:04:52 +0530

    Add ThemeProvider and useTheme hook for theme management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import CodeReview from "./pages/CodeReview";
 import NotFound from "./pages/NotFound";
@@ -6,32 +11,37 @@ import { ToastContainer } from "react-toastify";
 import Auth from "./pages/Auth";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import About from "./pages/About";
-import Contact from "./pages/Contact"
-
+import Contact from "./pages/Contact";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   const isAuthenticated = !!localStorage.getItem("token");
 
   return (
-    <Router>
-      <ToastContainer />
-      <Routes>
-        {/* Public Authentication Route */}
-        <Route path="/auth" element={isAuthenticated ? <Navigate to="/" replace /> : <Auth />} />
+    <ThemeProvider>
+      <Router>
+        <ToastContainer />
+        <Routes>
+          {/* Public Authentication Route */}
+          <Route
+            path="/auth"
+            element={isAuthenticated ? <Navigate to="/" replace /> : <Auth />}
+          />
 
-        {/* Protected Routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/review" element={<CodeReview />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/review" element={<CodeReview />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
 
-        {/* Default Redirect for Unmatched Routes */}
-        <Route path="*" element={<Navigate to="/auth" replace />} />
-      </Routes>
-    </Router>
+          {/* Default Redirect for Unmatched Routes */}
+          <Route path="*" element={<Navigate to="/auth" replace />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
